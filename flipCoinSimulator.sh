4a5c3b215f93f1cc5774347881b77cb8ab810a8e
rbhavitha <bhavitharedrouthu@gmail.com>
2020-10-06 23:13:49 +0530

    Display Head or Tail as Winner

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,3 +1,12 @@
 #!/bin/bash -x
 
 echo "Flip Coin Simulation"
+
+#uc1 display head or tail as winner
+FlipCoin=$(( $RANDOM%2 ))
+if [ $FlipCoin -eq 0 ]
+then
+	echo "Head is winner"
+else
+	echo "Tail is winner"
+fi
