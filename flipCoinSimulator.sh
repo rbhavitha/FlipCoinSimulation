57648bcaae7fa6451cc26b80f5aa1fa1e2dc121f
rbhavitha <bhavitharedrouthu@gmail.com>
2020-10-06 23:57:36 +0530

    show either of them win 21 times and show who won

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -2,7 +2,6 @@
 
 echo "Flip Coin Simulation"
 
-#uc1 display head or tail as winner
 FlipCoin=$(( $RANDOM%2 ))
 if [ $FlipCoin -eq 0 ]
 then
@@ -11,10 +10,9 @@ else
 	echo "Tail is winner"
 fi
 
-#uc2 show number of times head and tail has won
 headcount=0
 tailcount=0
-for (( i=1; i<=10; i++ ))
+while [[ $headcount -lt 21 && $tailcount -lt 21 ]]
 do
 	FlipCoin=$(( $RANDOM%2 ))
 	if [ $FlipCoin -eq 0 ]
@@ -28,3 +26,15 @@ do
 done
 echo "Number of times Heads has won:" $headcount
 echo "Number of times Tails has won:" $tailcount
+
+if [ $headcount -gt $tailcount ]
+then
+	echo "head won"
+	echo "head won by count of:" $headcount
+elif [ $headcount -lt $tailcount ]
+then
+	echo "tail won"
+	echo "tail won by count of:" $tailcount
+else
+	echo "tie"
+fi
